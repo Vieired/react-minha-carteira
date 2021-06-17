b4bd5c78bfcb99c3f80aed7919771dcd805a7454
egomes <egomes@metatron.com>
2021-06-17 20:51:04 -0300

    feat: implementa a exibição aleatória de emojis no elemento do perfil do usuário (colocar depois os emojis efetivamente)

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,14 +1,21 @@
 import React, { useMemo } from 'react';
 import { Container, Profile, Welcome, UserName } from './styles';
+import emojis from '../../utils/emojis';
 import Toggle from '../Toggle';
 
 const MainHeader: React.FC = () => {
 
+    const emoji = useMemo(() => {
+        const idx = Math.floor(Math.random() * emojis.length);
+        return emojis[idx];
+    },[]);
+    
     return (
         <Container>
-            <h1>MainHeader</h1>
             <Toggle />
+
             <Profile>
+                <Welcome>Olá, {emoji}</Welcome>
                 <UserName>Ednaldo Gomes</UserName>
             </Profile>
         </Container>
