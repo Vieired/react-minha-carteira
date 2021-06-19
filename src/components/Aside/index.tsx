a2c2f11170c2134c5594796a66620ed6b16fdb7d
egomes <egomes@metatron.com>
2021-06-19 20:36:23 -0300

    feat: cria menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+import {
+    Container,
+    Header,
+    LogImg,
+    MenuContainer,
+    MenuItemLink,
+    Title
+} from './styles';
+import { 
+    MdDashboard,
+    MdArrowUpward,
+    MdArrowDownward,
+    MdExitToApp
+} from 'react-icons/md';
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
+            <Header>
+                <LogImg src={logoImg} alt="Logo Minha Carteira" />
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>                                
+            </MenuContainer>
         </Container>
     );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,5 +4,48 @@ export const Container = styled.div`
     grid-area: AS;
     
     color: ${props => props.theme.colors.white};
-    background-color: ${props => props.theme.colors.secondary};    
+    background-color: ${props => props.theme.colors.secondary};
+    padding-left: 20px;
+    
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    height: 70px;
+    display: flex;
+    align-items: center;
+`;
+
+export const LogImg = styled.img`
+    height: 40px;
+    width: 40px;
+`;
+
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+
+    margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    margin: 7px 0;
+    transition: opacity .3s;
+    display: flex;
+    align-items: center;
+
+    > svg {
+        margin-right: 5px;
+    }
+
+    &:hover {
+        opacity: .7;
+    }
 `;
