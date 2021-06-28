d9060829f307a9b93c1d4b6d3c1eefa5aa339055
egomes <egomes@metatron.com>
2021-06-27 22:54:43 -0300

    fix: ajusta layout dos cards

--- src/components/HistoryFinanceCard/styles.ts
+++ src/components/HistoryFinanceCard/styles.ts
@@ -7,7 +7,7 @@ interface ITagProps {
 export const Container = styled.li`
     background-color: ${props => props.theme.colors.tertiary};
     list-style: none;
-    border-radius: 5px;
+    border-radius: 10px;
     margin: 10px 0;
     padding: 12px 10px;
     display: flex;
@@ -29,10 +29,15 @@ export const Container = styled.li`
         justify-contente: space-between;
         padding-left: 10px;
     }
+
+    > div span {
+        font-weight: 500;
+        font-size: 22px;
+    }
 `;
 
 export const Tag = styled.div<ITagProps>`
-    width: 10px;
+    width: 13px;
     height: 60%;
     background-color: ${props => props.color};
     position: absolute;
