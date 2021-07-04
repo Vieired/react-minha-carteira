05d7104319a817e55ffb807cdbb9c1f006d02051
egomes <egomes@metatron.com.br>
2021-07-04 14:06:28 -0300

    feat: implementa cards de movimentação financeira

---
+++ src/components/WalletBox/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+import dolarImg from '../../assets/dollar.svg';
+import arrowUpImg from '../../assets/arrow-up.svg';
+import arrowDownImg from '../../assets/arrow-down.svg';
+
+import { Container, Content } from './styles';
+
+interface IWalletBoxPropx {
+    title: string;
+    amount: number;
+    footerLabel: string;
+    icon: 'dolar' | 'arrowUp' | 'arrowDown';
+    color: string;
+}
+
+const WalletBox: React.FC<IWalletBoxPropx> = ({
+    title,
+    amount,
+    footerLabel,
+    icon, color
+}) => {
+
+    const iconSelected = () => {
+        switch(icon) {
+            case 'dolar': return dolarImg;
+            case 'arrowUp': return arrowUpImg;
+            case 'arrowDown': return arrowDownImg;
+            default: return dolarImg;
+        }
+    }
+
+    return (
+        <Container color={color}>
+            <span>{title}</span>
+            <h1>
+                <CountUp
+                    end={amount}
+                    prefix= {"R$ "}
+                    separator="."
+                    decimal=","
+                    decimals={2}
+                />
+            </h1>
+            <small>{footerLabel}</small>
+            <img src={iconSelected()} alt={title} />
+        </Container>
+    );
+}
+
+export default WalletBox;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,107 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import WalletBox from '../../components/WalletBox';
 
-import { Container } from './styles';
+import expenses from '../../repositories/expenses';
+import gains from '../../repositories/gains';
+import listOfMonths from '../../utils/months';
+
+import { Container, Content } from './styles';
 
 const Dashboard: React.FC = () => {
 
-    const options = [
-        { value: 'banana', label: 'Banana' },
-        { value: 'abacate', label: 'Abacate' },
-        { value: 'pêssego', label: 'Pêssego' },
-    ];
+    const [selectedMonth, setSelectedMonth] = useState<number>(new Date().getMonth()+1);
+    const [selectedYear, setSelectedYear] = useState<number>(2020);
+
+    const years = useMemo(() => {
+        let uniqueYears: number[] = [];
+
+        [...expenses, ...gains].forEach(item => {
+            const date = new Date(item.date);
+            const year = date.getFullYear();
+
+            if(!uniqueYears.includes(year)) {
+                uniqueYears.push(year)
+            }
+        });
+
+        return uniqueYears.map(year => {
+            return {
+                value: year,
+                label: year
+            }
+        });
+    },[]);
+
+    const months = useMemo(() => {
+        return listOfMonths.map((month, index) => {
+            return {
+                value: index + 1,
+                label: month,
+            }
+        });
+    },[]);    
+
+    const handleChangeMonth = (elem: React.ChangeEvent<HTMLSelectElement>) => {
+        try {
+            const parseMonth = Number(elem);
+            setSelectedMonth(parseMonth);
+        }
+        catch(eror) {
+            throw new Error('Invalid month value. Is accept 0 - 24.');
+        }
+    }
+
+    const handleChangeYear = (elem: React.ChangeEvent<HTMLSelectElement>) => {
+        try {
+            const parseYear = Number(elem);
+            setSelectedYear(parseYear);
+        }
+        catch(error) {
+            throw new Error('Invalid month value. Is accept 0 - 24.');
+        }        
+    }
 
     return (
         <Container>
             <ContentHeader title="Dashboard" lineColor="#F7931B">
-                <SelectInput options={options}/>
+                <SelectInput
+                    options={months}
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChangeMonth(e)}
+                    defaultValue={selectedMonth}
+                />
+                <SelectInput
+                    options={years}
+                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChangeYear(e)}
+                    defaultValue={selectedYear}
+                />
             </ContentHeader>
+
+            <Content>
+                <WalletBox
+                    title="saldo"
+                    color="#4E41F0"
+                    amount={150.00}
+                    footerLabel="Atualizado com base nas entradas e saídas"
+                    icon="dolar"
+                />
+                <WalletBox
+                    title="entradas"
+                    color="#F7931B"
+                    amount={5000.00}
+                    footerLabel="Atualizado com base nas entradas e saídas"
+                    icon="arrowUp"
+                />
+                <WalletBox
+                    title="saídas"
+                    color="#4E41F0"
+                    amount={4850.00}
+                    footerLabel="Atualizado com base nas entradas e saídas"
+                    icon="arrowDown"
+                />                                
+            </Content>            
         </Container>
     );
 }
