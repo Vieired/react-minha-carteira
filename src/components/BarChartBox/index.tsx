fbc2ddf3b63f5855872c22afaac0098f8967af1f
egomes <egomes@metatron.com.br>
2021-07-06 20:57:27 -0300

    refactor: torna o componente BarChartBox stateless

--- src/components/BarChartBox/index.tsx
+++ src/components/BarChartBox/index.tsx
@@ -28,44 +28,40 @@ interface IBarChartBoxProps {
 
 const BarChartBox: React.FC<IBarChartBoxProps> = ({
     title, data
-}) => {
-    return (
-        <Container>
+}) => (
+    <Container>
+        <SideLeft>
+            <h2>{title}</h2>
+            <LegendContainer>
+                {data.map(indicator => (
+                    <Legend color={indicator.color} key={indicator.name}>
+                        <div>{indicator.percent}%</div>
+                        <span>{indicator.name}</span>
+                    </Legend>
+                ))}
+            </LegendContainer>                
+        </SideLeft>
 
-            <SideLeft>
-                <h2>{title}</h2>
-                <LegendContainer>
-                    {data.map(indicator => (
-                        <Legend color={indicator.color} key={indicator.name}>
-                            <div>{indicator.percent}%</div>
-                            <span>{indicator.name}</span>
-                        </Legend>
-                    ))}
-                </LegendContainer>                
-            </SideLeft>
-
-            <SideRight>
-                <ResponsiveContainer>
-                    <BarChart data={data}>
-                        <Bar dataKey="amount" name="Valor">
-                            {data.map(indicator => (
-                                <Cell
-                                    key={indicator.name}
-                                    cursor="pointer"
-                                    fill={indicator.color}
-                                    
-                                />
-                            ))}
-                        </Bar>
-                        <Tooltip
-                            formatter={(value:number) => formatCurrency(value)}
-                            cursor={{fill:'none'}}/>
-                    </BarChart>
-                </ResponsiveContainer>
-            </SideRight>
-
-        </Container>
-    );
-}
+        <SideRight>
+            <ResponsiveContainer>
+                <BarChart data={data}>
+                    <Bar dataKey="amount" name="Valor">
+                        {data.map(indicator => (
+                            <Cell
+                                key={indicator.name}
+                                cursor="pointer"
+                                fill={indicator.color}
+                                
+                            />
+                        ))}
+                    </Bar>
+                    <Tooltip
+                        formatter={(value:number) => formatCurrency(value)}
+                        cursor={{fill:'none'}}/>
+                </BarChart>
+            </ResponsiveContainer>
+        </SideRight>
+    </Container>
+);
 
 export default BarChartBox;
