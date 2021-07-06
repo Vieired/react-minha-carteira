1dc4fb0c0a6ae5c159cda78ea27ffbb20de89789
egomes <egomes@metatron.com.br>
2021-07-06 20:58:10 -0300

    refactor: torna o componente Content stateless

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Container } from './styles';
 
-const Content: React.FC = ({ children }) => {
-    return (
-        <Container>
-            <h4>{ children }</h4>
-        </Container>
-    );
-}
+const Content: React.FC = ({ children }) => (
+    <Container>
+        <h4>{ children }</h4>
+    </Container>
+);
 
 export default Content;
