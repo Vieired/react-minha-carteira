497f3d8fd9f68c630ad4f3f6e39071d39af1ca37
egomes <egomes@metatron.com.br>
2021-07-07 20:50:31 -0300

    feat: cria tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const SignIn: React.FC = () => {
-    return (
-        <h1>SignIn</h1>
-    );
-}
+import logoImg from '../../assets/logo.svg';
+
+import {
+    Container,
+    Logo,
+    Form,
+    FormTitle
+} from './styles'
+
+const SignIn: React.FC = () => (
+    <Container>
+        <Logo>
+            <img src={logoImg} alt="Minha Carteira" />
+            <h2>Minha Carteira</h2>
+        </Logo>
+        <Form>
+            <FormTitle>Entrar</FormTitle>
+            <input type="text"></input>
+            <input type="text" />
+            <button type="submit">Acessar</button>
+        </Form>
+    </Container>
+);
 
 export default SignIn;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app.routes';
+// import App from './app.routes';
+import Auth from './auth.routes';
 
 const Routes: React.FC = () => (
     <BrowserRouter>
