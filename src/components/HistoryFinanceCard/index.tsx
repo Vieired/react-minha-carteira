3ce9f5e83c391794278c9cc062cc7b0cc2d3c79c
egomes <egomes@metatron.com.br>
2021-07-06 21:02:47 -0300

    refactor: torna o componente HistoryFinanceCard stateless

--- src/components/HistoryFinanceCard/index.tsx
+++ src/components/HistoryFinanceCard/index.tsx
@@ -14,18 +14,15 @@ const HistoryFinanceCard: React.FC<IHistoryFinanceCardProps> = ({
     title,
     subtitle,
     amount,    
-}) => {
-
-    return (
-        <Container>
-            <Tag color={tagColor} />
-            <div>
-                <span>{title}</span>
-                <small>{subtitle}</small>
-            </div>
-            <h3>{amount}</h3>
-        </Container>
-    );
-}
+}) => (
+    <Container>
+        <Tag color={tagColor} />
+        <div>
+            <span>{title}</span>
+            <small>{subtitle}</small>
+        </div>
+        <h3>{amount}</h3>
+    </Container>
+);
 
 export default HistoryFinanceCard;
