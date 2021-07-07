0b7cf1bb9a08e31ec744dc3754d8d6995c61262d
egomes <egomes@metatron.com.br>
2021-07-06 22:38:51 -0300

    feat: implementa troca do tema pelo botão Toggle

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -2,17 +2,35 @@ import React, { useMemo } from 'react';
 import { Container, Profile, Welcome, UserName } from './styles';
 import emojis from '../../utils/emojis';
 import Toggle from '../Toggle';
+import { useTheme } from '../../hooks/theme';
+import { useState } from 'react';
+import dark from '../../styles/themes/dark';
 
 const MainHeader: React.FC = () => {
+    const { toggleTheme, theme } = useTheme();
+
+    const [darkTheme, setDarkTheme] = useState(
+        () => theme.title === 'dark' ? true : false
+    );
 
     const emoji = useMemo(() => {
         const idx = Math.floor(Math.random() * emojis.length);
         return emojis[idx];
     },[]);
     
+    const handleChangeTheme = () => {
+        setDarkTheme(!darkTheme);
+        toggleTheme();
+    }
+
     return (
         <Container>
-            <Toggle />
+            <Toggle
+                labelLeft="Light"
+                labelRight="Dark"
+                checked={darkTheme}
+                onChange={handleChangeTheme}
+            />
 
             <Profile>
                 <Welcome>Olá, {emoji}</Welcome>
--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Container, ToggleLabel, ToggleSelector } from './styles';
 
-const Toggle: React.FC = () => (
+
+interface IToggleProps {
+    labelLeft: string;
+    labelRight: string;
+    checked: boolean;
+    onChange(): void;
+}
+
+const Toggle: React.FC<IToggleProps> = ({
+    labelLeft,
+    labelRight,
+    checked,
+    onChange
+}) => (
     <Container>
-        <ToggleLabel>Light</ToggleLabel>
+        <ToggleLabel>{labelLeft}</ToggleLabel>
         <ToggleSelector
-            checked
+            checked={checked}
             uncheckedIcon={false}
             checkedIcon={false}
-            onChange={() => console.log("Mudou o tema")} />
-        <ToggleLabel>Dark</ToggleLabel>
+            onChange={onChange}
+        />
+        <ToggleLabel>{labelRight}</ToggleLabel>
     </Container>
 )
 
