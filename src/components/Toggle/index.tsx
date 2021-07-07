c37d227efc1b6eca1a656530cafecf4634f3fadd
egomes <egomes@metatron.com.br>
2021-07-06 21:06:48 -0300

    refactor: torna o componente Toggle stateless

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Container, ToggleLabel, ToggleSelector } from './styles';
 
-const Toggle: React.FC = () => {
-    return (
-        <Container>
-            <ToggleLabel>Light</ToggleLabel>
-            <ToggleSelector
-                checked
-                uncheckedIcon={false}
-                checkedIcon={false}
-                onChange={() => console.log("Mudou o tema")} />
-            <ToggleLabel>Dark</ToggleLabel>
-        </Container>
-    )
-}
+const Toggle: React.FC = () => (
+    <Container>
+        <ToggleLabel>Light</ToggleLabel>
+        <ToggleSelector
+            checked
+            uncheckedIcon={false}
+            checkedIcon={false}
+            onChange={() => console.log("Mudou o tema")} />
+        <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+)
 
 export default Toggle;
