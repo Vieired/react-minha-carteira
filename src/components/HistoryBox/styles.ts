abc1be56d6dd83b73632670d4e2f105ac896a93b
egomes <egomes@metatron.com.br>
2021-07-08 22:29:48 -0300

    feat: aplica media queries no gráfico de histórico de saldos

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -34,6 +34,10 @@ export const Header = styled.header`
         margin-bottom: 20px;
         padding-left: 16px;
     }
+
+    @media(max-width: 1200px) {
+        flex-direction: column;      
+    }
 `;
 
 export const LegendContainer = styled.ul`
@@ -47,7 +51,7 @@ export const Legend = styled.li<ILegendProps>`
     align-items: center;
 
     margin-bottom: 7px;
-    margin-left: 7px;
+    margin-left: 16px;
 
     > div {
         background-color: ${props => props.color};
@@ -62,5 +66,13 @@ export const Legend = styled.li<ILegendProps>`
 
     > span {
         margin-left: 5px;
+    }
+
+
+    @media(max-width: 1280px) {
+        > div {
+            width: 30px;
+            height: 30px;
+        }
     }
 `;
