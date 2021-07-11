47b53c00be903e840709499155de7c1b07f4d534
egomes <egomes@metatron.com.br>
2021-07-11 18:50:01 -0300

    feat: aplica media querie no menu lateral

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,13 +1,35 @@
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
-export const Container = styled.div`
+
+interface IContainerProps {
+    menuIsOpen: boolean;
+}
+
+export const Container = styled.div<IContainerProps>`
     grid-area: AS;
     
-    color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
     padding-left: 20px;
     
     border-right: 1px solid ${props => props.theme.colors.gray};
+
+    position: relative;
+
+    @media(max-width: 600px) {
+        padding-left: 7px;
+        position: fixed;
+        z-index: 2;
+
+        width: 170px;
+
+        height: ${props => props.menuIsOpen ? '100vh' : '70px'};
+        overflow: hidden;
+
+        ${props => !props.menuIsOpen && css`
+            border: none;
+            border-bottom: 1px solid ${props => props.theme.colors.gray};
+        `};
+    }
 `;
 
 export const Header = styled.header`
@@ -19,11 +41,19 @@ export const Header = styled.header`
 export const LogImg = styled.img`
     height: 40px;
     width: 40px;
+
+    @media(max-width: 600px) {
+        display: none;
+    }      
 `;
 
 export const Title = styled.h3`
     color: ${props => props.theme.colors.white};
     margin-left: 10px;
+
+    @media(max-width: 600px) {
+        display: none;
+    }    
 `;
 
 export const MenuContainer = styled.nav`
@@ -55,17 +85,44 @@ export const MenuItemButton = styled.button`
     color: ${props => props.theme.colors.info};
     border: none;
     background: none;
-    text-decoration: none;
+    /* text-decoration: none; */
     margin: 7px 0;
-    transition: opacity .3s;
     display: flex;
     align-items: center;
 
-    > svg {
-        margin-right: 5px;
-    }
+    transition: opacity .3s;
 
     &:hover {
         opacity: .7;
+    }
+
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }    
+`;
+
+export const ToggleMenu = styled.button`
+    width: 40px;
+    height: 40px;
+
+    border-radius: 5px;
+    font-size: 22px;
+
+    background-color: ${props => props.theme.colors.warning};
+    color: ${props => props.theme.colors.white};
+
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .7;
+    }
+
+    display: none;
+
+    @media(max-width: 600px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;        
     }
 `;
