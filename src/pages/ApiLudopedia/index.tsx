484140712640e045818762dadf11c07c492dac86
Ednaldo Gomes <egomes@metatron.com.br>
2021-12-01 10:07:58 -0300

    fix: ajusta as dependências do hook useEffect

--- src/pages/ApiLudopedia/index.tsx
+++ src/pages/ApiLudopedia/index.tsx
@@ -5,45 +5,45 @@ import apiLudopedia from '../../services/ApiLudopedia';
 
 const ApiLudopedia: React.FC = () => {
 
-    const [items, setItems] = useState([]);
+    // const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    
-    const token = '6f655b678f09ac0f9f12e69d1fb255fa';
-    // const tokenGitlab = 'WyzSZGjXrXaaPccDJXGo';
-    const config = {
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            // method: 'GET',
-          //   'Authorization': `Basic ${token}`,
-          //   'Authorization': `token ${token}`,
-            // 'Authorization': `o-auth ${token}`
-            // 'Authorization': `o-auth2 ${token}`
-            // 'Content-Type': 'application/json',
-            // 'Accept': 'application/json',
-            // 'Vary': 'Accept-Encoding,Authorization,User-Agent'
-          //   'X-Requested-With': 'XMLHttpRequest'
-          //   'Access-Control-Allow-Origin': '*'
-          //   "Access-Control-Allow-Methods" : "GET",
-          //   "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"          
-        }
-    }
-    // apiLudopedia.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
-    // apiLudopedia.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-    // apiLudopedia.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-    // const cors = require("cors");
-    // apiLudopedia.options("*", cors({ origin: 'http://localhost:3000/api-ludopedia', optionsSuccessStatus: 200 }));
-    // apiLudopedia.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
-
-    // apiLudopedia.defaults.headers.Authorization = `Bearer ${token}`;
-
-    // apiLudopedia.defaults.baseURL = 'https://ludopedia.com.br/api/v1/';
-    // apiLudopedia.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    // apiLudopedia.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';    
-
-    
-    // GET JOGOS - LISTA DE JOGOS CADASTRADOS NO PORTAL
     useEffect(() => {
+        const token = '6f655b678f09ac0f9f12e69d1fb255fa';
+        // const tokenGitlab = 'WyzSZGjXrXaaPccDJXGo';
+        const config = {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                // method: 'GET',
+            //   'Authorization': `Basic ${token}`,
+            //   'Authorization': `token ${token}`,
+                // 'Authorization': `o-auth ${token}`
+                // 'Authorization': `o-auth2 ${token}`
+                // 'Content-Type': 'application/json',
+                // 'Accept': 'application/json',
+                // 'Vary': 'Accept-Encoding,Authorization,User-Agent'
+            //   'X-Requested-With': 'XMLHttpRequest'
+            //   'Access-Control-Allow-Origin': '*'
+            //   "Access-Control-Allow-Methods" : "GET",
+            //   "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"          
+            }
+        }
+        // apiLudopedia.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+        // apiLudopedia.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+        // apiLudopedia.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+        // const cors = require("cors");
+        // apiLudopedia.options("*", cors({ origin: 'http://localhost:3000/api-ludopedia', optionsSuccessStatus: 200 }));
+        // apiLudopedia.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
+
+        // apiLudopedia.defaults.headers.Authorization = `Bearer ${token}`;
+
+        // apiLudopedia.defaults.baseURL = 'https://ludopedia.com.br/api/v1/';
+        // apiLudopedia.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        // apiLudopedia.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';    
+
+        
+        // GET JOGOS - LISTA DE JOGOS CADASTRADOS NO PORTAL
         apiLudopedia.get(
             // '',
             "jogos",
@@ -123,7 +123,7 @@ const ApiLudopedia: React.FC = () => {
             <Content>
                 <a href="https://ludopedia.com.br/oauth?app_id=07051c2489ac83fd&redirect_uri=http://localhost:3000/api-ludopedia">Autorizar usuário na Ludopedia</a>
                 { isLoading && <Loading/> }
-                { !isLoading && console.log(items)}
+                {/* { !isLoading && console.log(items)} */}
             </Content>
         </Container>
     )
