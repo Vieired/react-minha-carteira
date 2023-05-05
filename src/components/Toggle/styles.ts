391e7481b34f7abb76f7f732641d30d092d932ae
Ednaldo Gomes <egomes@metatron.com.br>
2023-05-05 15:54:55 -0300

    fix(global): layout do componente toggle

--- src/components/Toggle/styles.ts
+++ src/components/Toggle/styles.ts
@@ -4,10 +4,12 @@ import Switch, { ReactSwitchProps } from 'react-switch';
 export const Container = styled.div`
     display: flex;
     align-items: center;
+    gap: 4px;
 `;
 
 export const ToggleLabel = styled.div`
     color: ${props => props.theme.colors.white};
+    font-size: 12px;
 `;
 
 export const ToggleSelector = styled(Switch).attrs<ReactSwitchProps>(
