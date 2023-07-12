c0c73953af5ea76da0219bd87f1fa338b22ce3c2
Ednaldo Gomes <egomes@metatron.com.br>
2023-07-12 19:10:41 -0300

    fix: componente InputMoney

--- src/components/Inputs/InputMoney/index.tsx
+++ src/components/Inputs/InputMoney/index.tsx
@@ -40,6 +40,35 @@ const InputMoney: React.FC<Props> = ({
 }) => {
     const [currencyValue, setCurrencyValue] = useState<string>(value);
 
+    const convertToAmericanNumberSystem = (val: string) => {
+        let newValue: string = val.replace(/\D/g,'');
+        newValue = (Number(newValue)/100).toFixed(2) + '';
+        newValue = newValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1');
+        val = newValue;
+        return newValue;
+    }
+
+    const formatMoney = (val: string) => {
+        // Aplica máscara de formato monetário apenas visualmente
+        // (o número no objeto deve permanecer em formato americano)
+
+        // Mascara com formato "locale pt-BR" e "currency BRL"
+        let newValue: string = val.replace(/\D/g,'');
+        newValue = (Number(newValue)/100).toFixed(2) + '';
+        newValue = newValue.replace(".", ",");
+        newValue = newValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+        val = newValue;
+
+        // // Mascara com formato monetário "locale en" e "currency USD"
+        // let newValue: string = val.replace(/\D/g,'');
+        // newValue = (Number(newValue)/100).toFixed(2) + '';
+        // newValue = newValue.replace(",", ".");
+        // newValue = newValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+        // val = newValue;
+
+        return setCurrencyValue(newValue);
+    }
+
     // const formatMoney = (val: string) => {
     //     // TODO: Melhorar esta função, pois ela só está aceitando até
     //     // 6 números, excluindo-se vírgulas e pontos   .     
@@ -55,38 +84,39 @@ const InputMoney: React.FC<Props> = ({
     //     return setCurrencyValue(valor);
     // }
 
-    const formatMoney = (val: string) => {
-        // TODO: Melhorar esta função, pois ela só está aceitando até
-        // 11 números, excluindo-se vírgulas e pontos.
-        let valor = val;
-        valor=valor.replace(/\D/g,"")  //permite digitar apenas número             
-        valor=valor.replace(/[0-9]{12}/,"inválido")   //limita pra máximo 999.999.999,99                
-        valor=valor.replace(/(\d{1})(\d{8})$/,"$1.$2")  //coloca ponto antes dos últimos 8 dígitos     
-        valor=valor.replace(/(\d{1})(\d{5})$/,"$1.$2")  //coloca ponto antes dos últimos 5 dígitos                
-        valor=valor.replace(/(\d{1})(\d{1,2})$/,"$1,$2")    //coloca virgula antes dos últimos 2 dígitos                
-        val = valor;   
+    // const formatMoney = (val: string) => {
+    //     // TODO: Melhorar esta função, pois ela só está aceitando até
+    //     // 11 números, excluindo-se vírgulas e pontos.
+    //     let valor = val;
+    //     valor=valor.replace(/\D/g,"")  //permite digitar apenas número             
+    //     valor=valor.replace(/[0-9]{12}/,"inválido")   //limita pra máximo 999.999.999,99                
+    //     valor=valor.replace(/(\d{1})(\d{8})$/,"$1.$2")  //coloca ponto antes dos últimos 8 dígitos     
+    //     valor=valor.replace(/(\d{1})(\d{5})$/,"$1.$2")  //coloca ponto antes dos últimos 5 dígitos                
+    //     valor=valor.replace(/(\d{1})(\d{1,2})$/,"$1,$2")    //coloca virgula antes dos últimos 2 dígitos                
+    //     val = valor;   
       
-        return setCurrencyValue(valor);
-    }
+    //     return setCurrencyValue(valor);
+    // }
 
     const handleChange = (e: React.ChangeEvent<any>) => {
-        setCurrencyValue(e.target.value || '');
+        // Converte o número puro que vem do evento para um número em formato americano
+        const number = convertToAmericanNumberSystem(e.target.value);
+        setCurrencyValue(number || '');
         onChange && onChange({
             ...e,
             target: {
                 ...e.target,
-                value: currencyValue || '',
+                value: number || '',
                 id,
                 name
             }
         } as React.ChangeEvent<any>)        
-    }
+    }    
 
     return (
         <Container className={className}>
             { label && <label htmlFor={id || name}>{label}</label> }
             <input
-                // value={value == null ? "" : value}
                 value={currencyValue == null ? "" : currencyValue}
                 id={id || name}
                 name={name}
