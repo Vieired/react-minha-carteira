0e86fe4ca6d209acee3e7d2cc13513fe03935de8
Ednaldo Gomes <egomes@metatron.com.br>
2023-08-01 15:10:17 -0300

    feat: implementa Suspense (falta o lazy)

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from '../components/Layout';
+import { BigSpinner } from '../components/BigSpinner';
 import Dashboard from '../pages/Dashboard';
 import ApiSw from '../pages/ApiSw';
 import ApiGitLab from '../pages/ApiGitLab';
@@ -14,13 +15,28 @@ import EditBudget from '../pages/BudgetManagement/EditBudget';
 const AppRoutes: React.FC = () => (
     <Layout>
         <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/apisw" exact component={ApiSw} />
-            <Route path="/apigitlab" exact component={ApiGitLab} />
-            <Route path="/list/:type" exact component={List} />
-            <Route path="/budget" exact component={BudgetManagement} />
-            <Route path="/addbudget" exact component={AddBudget} />
-            <Route path="/budget/edit/:id" exact component={EditBudget} />
+            <Suspense fallback={<BigSpinner/>}>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/apisw" exact component={ApiSw} />
+                <Route path="/apigitlab" exact component={ApiGitLab} />
+                <Route path="/list/:type" exact component={List} />
+                <Route path="/budget" exact component={BudgetManagement} />
+                    {/* <Route path="/budget" exact component={BudgetManagement} /> */}
+                <Route path="/addbudget" exact component={AddBudget} />
+                <Route path="/budget/edit/:id" exact component={EditBudget} />
+            </Suspense>
+
+            {/* <Route path="/budget" component={BudgetManagement}>
+                <Route path="add" component={AddBudget} />
+                <Route path="edit/:id" component={EditBudget} />
+            </Route> */}
+
+            {/* <Route path="/budget" component={BudgetManagement} children={
+                <>
+                    <Route path="add" component={AddBudget} />
+                    <Route path="edit/:id" component={EditBudget} />                
+                </>
+            }/> */}
         </Switch>
     </Layout>
 );
