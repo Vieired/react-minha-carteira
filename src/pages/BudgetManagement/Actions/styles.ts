b15da38d103da141ac400bde6d39d7fe5d0a1a14
Ednaldo Gomes <egomes@metatron.com.br>
2023-08-02 11:13:15 -0300

    feat(budget-management): implementa Acessibilidade aos botões de ação da tabela

--- src/pages/BudgetManagement/Actions/styles.ts
+++ src/pages/BudgetManagement/Actions/styles.ts
@@ -4,11 +4,23 @@ export const Container = styled.div`
     display: flex;
     column-gap: 16px;
 
+    /* &:focus-within {
+        border: solid blue 3px;
+    } */
+
     a {
         color: ${props => props.theme.colors.white};
+        border-radius: 6px;
+        outline: 4px transparent #91caff;
 
         &:link, &:hover, &:visited, &:active {
             color: ${props => props.theme.colors.gray};
+        }
+
+        &:focus {
+            outline: 4px solid #91caff;
+            outline-offset: 1px;
+            transition: outline-offset 0s,outline 0s;
         }
     }
 
