7c67e465452d89acbb5789d9449a3deff19c99e6
Ednaldo Gomes <egomes@metatron.com.br>
2024-07-11 12:35:48 -0300

    fix(budget): form layout

--- src/pages/BudgetManagement/EditBudget/styles.ts
+++ src/pages/BudgetManagement/EditBudget/styles.ts
@@ -3,6 +3,12 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+
+    > form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
 `
 
 export const Buttons = styled.div`
